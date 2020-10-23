683797ffe4b638721482b63cf684b351f7d0c1b8
이지영 <leejyart@leejyart.local>
2020-10-23 21:34:52 +0900

    Add: first commit

--- src/pages/MainPage/components/ProductRecommendation/ProductRecommendation.js
+++ src/pages/MainPage/components/ProductRecommendation/ProductRecommendation.js
@@ -4,8 +4,15 @@ import "./ProductRecommendation.scss";
 class ProductRecommendation extends Component {
   render() {
     return (
-      <div>
-        <h1>h1</h1>
+      <div className="Container">
+        <div className="title">
+          <h2>
+            New <br /> Arrivals
+          </h2>
+        </div>
+        <div className="topArea">
+          <div className=""></div>
+        </div>
       </div>
     );
   }
