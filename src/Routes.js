1e19b41928a1e9f3e46a785800448a4de84dedc7
이지영 <leejyart@leejyart.local>
2020-10-26 15:56:01 +0900

    no meaning

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
+import DetailPage from "./pages/DetailPage/DetailPage";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={MainPage} />
+          <Route exact path="/" component={DetailPage} />
         </Switch>
       </Router>
     );
